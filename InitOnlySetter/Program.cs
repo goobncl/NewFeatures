f2aa44b6bc5f25caeaca6a97ffa1ae10d6e9c675
user <goobncl@gmail.com>
2021-08-19 23:00:22 +0400

    first commit

---
+++ InitOnlySetter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InitOnlySetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Init Only Setter Porject");
+        }
+    }
+}
