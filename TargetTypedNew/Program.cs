b8d82167dab80869f37531f8ae53a42f3e8fb8b8
user <goobncl@gmail.com>
2021-08-20 21:54:41 +0400

    modify Target Typed code

--- TargetTypedNew/Program.cs
+++ TargetTypedNew/Program.cs
@@ -1,29 +1,9 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
 
 namespace TargetTypedNew
 {
     class Program
     {
-        static double GenerateSummary(string[] args)
-        {
-            string formatString = "Format String";
-            GenerateOrderReport(() =>
-            {
-                return formatString;
-            });
-        }
-
-        static string GenerateOrderReport(Func<string> getFormatString)
-        {
-            var order = new
-            {
-                Orderid = 1,
-                OrderDate = DateTime.Now
-            };
-            return string.Format(getFormatString(), order.Orderid);
-        }
-
         static void main(string[] args)
         {
             // With target typed expression, the type StringBuilder is inferred from the context
