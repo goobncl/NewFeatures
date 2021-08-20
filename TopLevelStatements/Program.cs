8a707323375bb8f592861e1f050a648552a5f9dd
user <goobncl@gmail.com>
2021-08-20 14:58:52 +0400

    Add Top Level Statements code

---
+++ TopLevelStatements/Program.cs
@@ -0,0 +1,8 @@
+﻿
+// With top level statements, the ceremony code required at the application initialization can be omitted
+System.Console.WriteLine("Hello World!");
+
+foreach (var v in args)
+{
+    System.Console.WriteLine(v);
+}
