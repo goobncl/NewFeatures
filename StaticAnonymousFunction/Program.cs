2412b3bf27b7069281657895fcbe42394779f96c
user <goobncl@gmail.com>
2021-08-21 21:46:16 +0400

    Add Static Anonymous Function project

---
+++ StaticAnonymousFunction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace StaticAnonymousFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        string formatString = $"{0}-{1}";
+
+        void GenerateSummary(string[] args)
+        {
+            string result = GenerateOrderReport(static () => 
+            {
+                // return formatString; // Will get error
+                return $"Order Id: {1}, Order Date: {1}";
+            });
+            Console.WriteLine(result);
+        }
+
+        static string GenerateOrderReport(Func<string> getFormatString)
+        {
+            var order = new
+            {
+                Orderid = 1,
+                OrderDate = DateTime.Now
+            };
+            return string.Format(getFormatString(), order.Orderid);
+        }
+    }
+}
