671dd50d52eb58b08d222649c024b020971cb330
user <goobncl@gmail.com>
2021-08-21 22:21:12 +0400

    Add the code snippet about yield keyword

--- StaticAnonymousFunction/Program.cs
+++ StaticAnonymousFunction/Program.cs
@@ -1,20 +1,47 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Collections;
 
 namespace StaticAnonymousFunction
 {
+    public class MyList
+    {
+        private int[] data = { 1, 2, 3, 4, 5 };
+
+        public IEnumerator GetEnumerator()
+        {
+            int i = 0;
+            while (i < data.Length)
+            {
+                yield return data[i];
+                i++;
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var list = new MyList();
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            IEnumerator it = list.GetEnumerator();
+            it.MoveNext();
+            Console.WriteLine(it.Current);
+            it.MoveNext();
+            Console.WriteLine(it.Current);
         }
 
         string formatString = $"{0}-{1}";
 
         void GenerateSummary(string[] args)
         {
-            string result = GenerateOrderReport(static () => 
+            string result = GenerateOrderReport(static () =>
             {
                 // return formatString; // Will get error
                 return $"Order Id: {1}, Order Date: {1}";
@@ -31,5 +58,14 @@ namespace StaticAnonymousFunction
             };
             return string.Format(getFormatString(), order.Orderid);
         }
+
+        /*public static IEnumerable<string> ReadLineByLine(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName)) throw new ArgumentNullException(nameof(fileName));
+            foreach (var line in File.ReadAllLines(fileName))
+            {
+                yield return line;
+            }
+        }*/
     }
 }
