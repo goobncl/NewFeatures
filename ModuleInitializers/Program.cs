ecffeffd5b1495b803fdd27f69ac94237aa5a79c
user <goobncl@gmail.com>
2021-08-24 20:13:18 +0400

    Add Module Initializer project

---
+++ ModuleInitializers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ModuleInitializers
+{
+    class Program
+    {
+        /// <summary>
+        /// This function get called when assembly is loaded.
+        /// </summary>
+        [ModuleInitializer]
+        internal static void Initialize()
+        {
+            Console.WriteLine("Module Initialization");
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
