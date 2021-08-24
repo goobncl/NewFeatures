a9c830db599d8e9fa75e585c30f5ff50de985a6f
user <goobncl@gmail.com>
2021-08-24 20:02:43 +0400

    It got complicated in my head and I started over

--- StaticAnonymousFunction/Program.cs
+++ StaticAnonymousFunction/Program.cs
@@ -1,58 +1,12 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
-using System.Collections;
 
 namespace StaticAnonymousFunction
 {
-    public class MyList
-    {
-        private int[] data = { 1, 2, 3, 4, 5 };
-
-        public IEnumerator GetEnumerator()
-        {
-            int i = 0;
-            while (i < data.Length)
-            {
-                yield return data[i];
-                i++;
-            }
-        }
-    }
-
-    public class Demo
-    {
-        private const string formattedText = "{0} It was developed by Microsoft's Anders Hejlsberg in the year 2000.";
-        
-        void DisplayText(Func<string, string> func)
-        {
-            Console.WriteLine(func("C# is a popular programming language."));
-        }
-
-        public void Display()
-        {
-            DisplayText(static text => string.Format(formattedText, text));
-        }
-    }
-
     class Program
     {
-        static void Main(string[] args)
+        static void main(string[] args)
         {
-
-            new Demo().Display();
-
-            /*var list = new MyList();
-
-            foreach (var item in list)
-            {
-                Console.WriteLine(item);
-            }
-
-            IEnumerator it = list.GetEnumerator();
-            it.MoveNext();
-            Console.WriteLine(it.Current);
-            it.MoveNext();
-            Console.WriteLine(it.Current);*/
 
         }
 
@@ -60,12 +14,11 @@ namespace StaticAnonymousFunction
 
         void GenerateSummary(string[] args)
         {
-            string result = GenerateOrderReport(static () =>
+            GenerateOrderReport(static () => 
             {
                 // return formatString; // Will get error
-                return $"Order Id: {1}, Order Date: {1}";
+                return $"Order Id:{0}, Order Date: {1}";
             });
-            Console.WriteLine(result);
         }
 
         static string GenerateOrderReport(Func<string> getFormatString)
@@ -77,14 +30,5 @@ namespace StaticAnonymousFunction
             };
             return string.Format(getFormatString(), order.Orderid);
         }
-
-        /*public static IEnumerable<string> ReadLineByLine(string fileName)
-        {
-            if (string.IsNullOrEmpty(fileName)) throw new ArgumentNullException(nameof(fileName));
-            foreach (var line in File.ReadAllLines(fileName))
-            {
-                yield return line;
-            }
-        }*/
     }
 }
